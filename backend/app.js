7a5117c5ebaa6c9a332ce730ba03115a485bd217
vijithhg <vijithhgedu@gmail.com>
2024-09-18 20:30:17 +0530

    app

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 
+app.get('/users',(req,res)=>{
+    res.send([
+        {
+        name:'John',
+        age:20
+        },
+        {
+            name:'Smith',
+            age:25
+            },
+            {
+                name:'Doe',
+                age:30
+                },
+])
+})
 
 app.listen(8000,()=>{
     console.log('Server running successfully')
